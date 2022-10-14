a9bb2c3668afb0e7e90dcf3c56efbb1328c27eaf
Guillaume <myw4tch@gmail.com>
2022-10-14 15:15:49 +0200

    make calendar selectable

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar, { DateSelectArg } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
 function App() {
+
+  const handleDateSelect = (date: DateSelectArg) => {
+    console.log(date);
+  }
+  
   return (
       <FullCalendar
         plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
         initialView="timeGridWeek"
+        selectable={true}
+        selectMirror={true}
+        select={handleDateSelect}
       />
   );
 }
