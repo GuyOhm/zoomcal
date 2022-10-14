0a36326506c81567f3605302dba8627eafb7a1f6
Guillaume <myw4tch@gmail.com>
2022-10-14 15:33:07 +0200

    load and display events

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FullCalendar, { DateSelectArg } from "@fullcalendar/react";
+import FullCalendar, { DateSelectArg, EventContentArg, EventSourceInput } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
@@ -9,11 +9,25 @@ function App() {
   const handleDateSelect = (date: DateSelectArg) => {
     console.log(date);
   }
-  
+
+  const loadEvents = (): EventSourceInput => {
+    return [
+      {
+        title: 'event 1',
+        start: "2022-10-12T06:30:00.000Z",
+        end: "2022-10-12T08:00:00.000Z",
+      }
+    ]
+  }
+
+
+
   return (
       <FullCalendar
         plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
         initialView="timeGridWeek"
+        initialEvents={loadEvents()}
+        eventContent={renderEventContent}
         selectable={true}
         selectMirror={true}
         select={handleDateSelect}
@@ -22,3 +36,13 @@ function App() {
 }
 
 export default App;
+
+function renderEventContent(event: EventContentArg) {
+  return (
+    <>
+      <b>{event.timeText} - </b>
+      <i>{event.event.title}</i>
+    </>
+  )
+}
+
